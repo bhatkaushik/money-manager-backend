251365a6cc0eb0d47d262f381437bcca9c8b7e31
Kaushik G Bhat <91201263+bhatkaushik@users.noreply.github.com>
2022-11-19 20:22:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cors());
 
 //routes
-app.get("/",(req,res)=>{res.send(data:"hello")})
+app.get("/",(req,res)=>{res.send("hello")})
 //transections routes
 app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 //port
